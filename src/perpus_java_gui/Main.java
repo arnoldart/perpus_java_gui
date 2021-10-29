d9067734443f927924825ddbe7a008abe2a976b5
arnoldart <adoni.091203@gmail.com>
2021-10-29 09:27:50 +0700

    menambahkan sistem login

--- src/perpus_java_gui/Main.java
+++ src/perpus_java_gui/Main.java
@@ -1,10 +1,10 @@
-
 package perpus_java_gui;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+        LoginSystem form = new LoginSystem();
+        form.setVisible(true);
     }
 
 }
